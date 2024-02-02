6f466b31b44c343e09d0772daec35419381847e2
Eldana <kadyelyer@gmail.com>
2024-02-02 21:21:05 +0600

    Initial Commit

---
+++ shift test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shift test
+//
+//  Created by eldana on 02.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
